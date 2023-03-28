24415b84f39d28d44aa58bf844489e4d2dfe99af
sunil_tomar <sunil_tomar@persistent.com>
2023-03-29 00:37:11 +0530

    added ADD Expanse API
    added ADD Expanse API

--- accounts/src/component/Accounts/Accounts.js
+++ accounts/src/component/Accounts/Accounts.js
@@ -5,39 +5,96 @@ import DateObject from "react-date-object";
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import {data_fetch_account_list} from './../../DataSet/DataSet';
-
+import ReactTable from "react-table-6";  
+import "react-table-6/react-table.css" 
+import { Form } from 'react-bootstrap';
+//Server URL 
+const serverUrl="http://localhost:9050/";
 
 class Accounts extends React.Component {
-
- constructor(){
-  super();
-  this.state = {
-     items: data_fetch_account_list,//[ {  id: 1,  paidFor: "Suraj",  amount: "10000",  createdTime: "1679159469870" }, {  id: 12, paidFor: "Sunil",  amount: "2000000", createdTime: "1679159469870" },  ],
-     isLoaded:false,
-     error:"",
-     countCall:0,
-     totoalAmount:0  
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: data_fetch_account_list,
+      isFormOpen: false,
+      isLoaded:false,
+      error:"",
+      countCall:0,
+      totoalAmount:0  
     };
 }
+
+handleToggleForm = () => {
+  console.log(" handleToggleForm ");
+  this.setState({ isFormOpen: !this.state.isFormOpen });
+};
+
+
+handleSubmit = (event) => {
+  console.log(" handleSubmit ");
+
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const formData = {
+    paidFor: data.get('paidFor'),
+    amount: data.get('amount'),
+  };
+  fetch(//serverUrl+'monthly-expense/add',{
+       'http://localhost:9050/monthly-expense/add', {
+    method: 'POST',
+    body: JSON.stringify(formData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(response => response.json())
+  .then(data => {
+    debugger;
+    //let saveedEnity=data.entity;
+    console.log(data);
+    this.setState({ isFormOpen: false, items : [this.state.items + data] });
+  })
+  .catch(error => console.error(error));
+};
+
+
 componentDidMount(){
   //call api for fetching Data.
-  console.log("api call");
+  console.log("inactive api call");
   this.fetchAccountsData();
-}
+} 
 
  addExpanse=()=>{
   console.log("AddExpanse");
+  const newItem = { id: 400, paidFor: 'New User', amount: 400, createdTime: "1679159469870"};
+  console.log("adding newItem : ");
+  console.log(newItem);
+  this.setState({ items: [...this.state.items, newItem] });
   //alert("Hi Add");
-return(<div><h2>Hi All</h2></div>);
+  return(<div><h2>Hi All</h2></div>);
 }
 
-  render() {
+render() {
     //return template.call(this);
     return <div>
      <h2 className="title_head">Hi Accounts Page welcomes you</h2>
     <center>
      <div className="accounts-table-div">
-     <span > <Button onClick={this.addExpanse}>New BTN</Button>  </span>
+     <>
+      {this.state.isFormOpen &&
+        <div className="form-popup">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Reason" name="paidFor" />
+            <input type="text" placeholder="Money" name="amount" />
+            <button type="submit">Submit</button>
+            <button type="button" onClick={this.handleToggleForm}>Cancel</button>
+          </form>
+        </div>
+      }
+      <button onClick={this.handleToggleForm}>Add Expanse</button>
+    </>
+
       <Table id="accounts-table"striped bordered hover>
        <thead>
          <tr>
@@ -62,7 +119,7 @@ return(<div><h2>Hi All</h2></div>);
           <td style={{'fontSize':'2rem', 'textAlign':'right'}}><b>Total Amount : { (this.state.items.reduce((total, item)=>total+item.amount, 0)) }</b></td>
           </tr>
        </tbody>
-     </Table> 
+     </Table>  
      </div></center>
   </div>;
   }
@@ -96,5 +153,5 @@ export default Accounts;
 
 const addExpanseModalForm=()=>{
   console.log("hey modal open.");
- alert("hey modal open..");
+   alert("hey modal open..");
 }
