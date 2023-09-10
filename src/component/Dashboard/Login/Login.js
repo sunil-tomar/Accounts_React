8d4600fa9234b43e814457da70677c3ef39d3a65
suniltomar <suniltomar.developer@gmail.com>
2023-09-10 22:37:15 +0530

    feat - Login is working for every page.
    feat - Login is working for every page.

--- src/component/Dashboard/Login/Login.js
+++ src/component/Dashboard/Login/Login.js
@@ -1,28 +1,67 @@
 import React, { useState } from 'react';
 import './Login.css'
+import axios from 'axios';
+//import {SERVER_URL, URL_ADD_OR_UPDATE, URL_FETCH_NEXT_CHUNK, URL_MONTHLY_EXPANSE } from '../../utils/URL_CONSTANT';
 
 function Login({ onLogin }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
   const [error, setError] = useState('');
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Simulate authentication logic (replace with your actual authentication)
-    // You can add your login logic here
-    if (username === 'sunil' && password === 'test') {
-      alert('Login successful'); setError('');
-      // Call the onLogin callback to notify the parent component of the successful login
-      onLogin();
-    } else {
-      alert('Login failed. Please check your credentials.'); 
-      setError('Invalid username or password. Please try again.');
-    }
-  };
+  
+     const serverLogin = () => {
+        const BASEURL = "http://localhost:5050/mastertemplate/api/user/restapi-login";
+        axios.post(BASEURL,
+                  {"username": username,"password": password}, 
+                  {headers: { "Access-Control-Allow-Origin": "*"}}
+        ).then((resp)=>{
+         // console.log(resp);
+          let headersObj=resp.headers;
+          console.log(headersObj);
+          if (typeof resp.data === "object" && (resp.data.success==="true" || resp.data.success===true)) { 
+          let token=resp.data.token;
+            setToken(token);
+            onLogin();  
+            alert('Login successful'); setError('');
+          }else{
+            alert('Login failed. Please check your credentials.'); 
+            setError('Invalid username or password. Please try again.');
+          }
+        })
+        .catch((err)=>{
+          alert('Login failed. Please check your credentials.'); 
+          setError('Invalid username or password. Please try again.');
+          console.error(err)
+            setToken("");
+           // onLogin(false);
+        });
+        
+      }
 
-  return (
+      //login
+      serverLogin();
+
+      const getData = () => {
+        console.debug("get-Data");
+      const BASEURL = "http://localhost:5050/mastertemplate/api/user/";
+      axios.get(BASEURL, { headers: { 'Authorization': `Bearer `+token}, "Access-Control-Allow-Origin": "*"})
+      .then((resp)=>{
+        console.log(resp)
+        debugger;
+      })
+      .catch((err)=>{console.error(err)});
+      }
+
+      
+      getData();
+
+
+  };
+return (
     <div className="login-container">
-      <h2>Login</h2>
+      <h2>Login {error}</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Username:</label>
@@ -47,5 +86,21 @@ function Login({ onLogin }) {
     </div>
   );
 }
-
 export default Login;
+
+
+
+/*
+const loginAPI = () => {
+  alert("get-login");
+  const url = "http://localhost:5050/mastertemplate/api";
+  const requestOptions = {
+      method: 'POST',
+      headers: {"Access-Control-Allow-Origin": url,'Content-Type': 'application/json'},
+      body: JSON.stringify({'username': 'sunil', 'password':'test'})
+  }
+  fetch(url+"/user/login", requestOptions)
+  .then(response => console.log('Submitted successfully'))
+  .catch(error => console.log('Form submit error', error))
+    alert("Thank you for contacting Accounts App. We will contact you shortly.");
+}*/
